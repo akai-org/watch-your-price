c0583bf49e1b80d8db710dfb8e370a07d21ee3b4
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-04-19 19:32:34 +0200

    add product link to serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 
-from products.models import Products
+from products.models import Product
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Products
-        fields = ['id', 'name', 'price']
+        model = Product
+        fields = ['id', 'name', 'link', 'price']
