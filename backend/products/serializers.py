795cb1faf3ac9084a90b4cdf9505a10693495866
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-04-19 19:34:44 +0200

    change class name for product serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from products.models import Product
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'link', 'price']
