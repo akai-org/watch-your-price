27be0b070826c77054ca83757ce0123cbf25f935
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-04-19 19:54:50 +0200

    add view set for products

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,12 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from products import views
 
 admin.autodiscover()
 admin.site.enable_nav_sidebar = False
 
+router = routers.DefaultRouter()
+router.register(r'products', views.ProductViewSet)
 
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
