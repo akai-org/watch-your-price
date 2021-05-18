c355a1e59e39d613778fbef91116d09cc46d5545
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-18 16:36:55 +0200

    get request params explicitly

--- backend/search/views.py
+++ backend/search/views.py
@@ -8,8 +8,18 @@ from rest_framework.views import APIView
 class SearchProducts(APIView):
 
     def get(self, request):
+
+        params = {}
+
         try:
-            response = requests.get('http://localhost:8001/search', params=request.query_params)
+            params['phrase'] = request.query_params['phrase']
+            params['website'] = request.query_params['website']
+        except KeyError:
+            return HttpResponseBadRequest()
+        params['page'] = request.query_params['page'] if 'page' in request.query_params else 0
+
+        try:
+            response = requests.get('http://localhost:8001/search', params=params)
             response.raise_for_status()
         except requests.exceptions.RequestException:
             return HttpResponseBadRequest()
