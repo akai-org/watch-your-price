c1d44554d938a360eb47e3b6a523580fbcbf7eb0
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-18 14:55:32 +0200

    get params from query

--- backend/search/views.py
+++ backend/search/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseBadRequest
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -6,4 +7,10 @@ from rest_framework.views import APIView
 class SearchProducts(APIView):
 
     def get(self, request):
-        return Response({"search": "result"}, status=status.HTTP_200_OK)
+        try:
+            phrase = request.query_params['phrase']
+            website = request.query_params['website']
+        except KeyError:
+            return HttpResponseBadRequest()
+        page = request.query_params['page'] if 'page' in request.query_params else 0
+        return Response(request.query_params, status=status.HTTP_200_OK)
